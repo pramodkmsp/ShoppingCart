c7b0da5549ce1f55447edf71542ec871b0fbad95
pramodkmsp <pramodkmsp1995@gmail.com>
2020-08-14 22:55:15 +0530

    Api endpoint changed

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -1,6 +1,6 @@
 import { environment } from 'src/environments/environment';
 
-export const baseUrl = environment.production ? 'https://api.shoppingcart.com' : 'http://localhost:3000';
+export const baseUrl = environment.production ? 'https://my-json-server.typicode.com/pramodkmsp/shoppingcart-json-server' : 'https://my-json-server.typicode.com/pramodkmsp/shoppingcart-json-server';
 export const productUrl = baseUrl + '/products';
 export const cartUrl = baseUrl + '/cart';
 export const wishlistUrl = baseUrl + '/wishlist';
